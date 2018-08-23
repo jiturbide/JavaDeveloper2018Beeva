0cf54c61d6c129116e035818b37cc522086a4de7
Jose Luis Iturbide <j_iturbide@hotmail.com>
2018-08-22 20:07:49 -0500

    20180823 JLIL Clase base Ejercicio DEsordenar

---
+++ M01C01/wksp/M01C01Lesson10/src/com/curso/Desordenamiento.java
@@ -0,0 +1,23 @@
+package com.curso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// size(), add(index, object), remove(Object), isEmpty() 
+public class Desordenamiento {
+    public static void main(String[] args) {
+        List <Integer>lista = new ArrayList<Integer>();
+        for (int i = 0; i < 50; i++) {
+            lista.add(i);
+        }
+        
+        List desordenada = desordenar(lista);
+        System.out.println("Desordenada: " + desordenada);
+    }
+    
+    public static List desordenar(List lista) {
+        
+        
+        return null;
+    }
+}

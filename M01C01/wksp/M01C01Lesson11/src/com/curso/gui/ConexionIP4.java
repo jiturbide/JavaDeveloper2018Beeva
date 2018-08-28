b46ee3e63228924ed7bd442e86d787a165e9ce3d
Jose Luis Iturbide <j_iturbide@hotmail.com>
2018-08-27 22:10:04 -0500

    20180822 JLIL Codigo para tema de Herencia

---
+++ M01C01/wksp/M01C01Lesson11/src/com/curso/gui/ConexionIP4.java
@@ -0,0 +1,30 @@
+package com.curso.gui;
+
+/**
+ * Crear una clase ConexionIP4 a la que se le pueda proporcionar 3 Objetos IP que seran miembros internos
+ * Se daran valores a cada IP usando el metodo setIp(TipoIpEnum, seg01, seg02, seg03, seg04)
+ * Se devolvera la IP con el metodo getIp(TipoIpEnum) : Ip
+ * Cuando se liste toString de la clase debe imprimir el texto que corresponda
+   
+   IPv4 Address: 172.20.1.45
+   Subnet Mask : 255.255.255.0
+   Gateway: 172.20.1.1
+    
+ * Curso Java
+ * @author jose.iturbide
+ */
+public class ConexionIP4 {
+    public static void main(String[] args) {
+        /*
+          ConexionIP4 cnx = new ConexionIP4();
+          cnx.setIp(TipoIpEnum.IPV4, 172,20,1,45);
+          cnx.setIp(TipoIpEnum.MASK, 255,255,255,0);
+          cnx.setIp(TipoIpEnum.GATEWAY, 172,20,1,1);
+        
+          Ip ip = cnx.getIp(); //Ip es inmutable, sin embargo tiene metodos set()
+          System.out.println(ip);
+          System.out.println(cnx);
+          
+        */
+    }
+}
---
+++ M01C01/wksp/M01C01Lesson11/src/com/curso/gui/TestClasesInternas.java
@@ -0,0 +1,9 @@
+package com.curso.gui;
+
+/**
+ * Curso Java
+ * @author jose.iturbide
+ */
+public class TestClasesInternas {
+
+}
